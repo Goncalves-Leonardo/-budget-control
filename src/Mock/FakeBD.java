2da26a1027ead7d1d9dc31d78d27e12fee7243d5
Leonardo Gonçalves <leogonsil97@gmail.com>
2020-07-26 16:54:09 -0300

    add toString method

--- src/Mock/FakeBD.java
+++ src/Mock/FakeBD.java
@@ -7,4 +7,23 @@ import models.entities.Account;
 
 public class FakeBD {
 	public List<Account> accs = new ArrayList<>();
+	
+	@Override
+	public String toString() {
+		
+		String format = "";
+		
+		for(Account acc : accs) {
+			format += acc.getName() 
+					+ " - " 
+					+ acc.getType().getValue() 
+					+ " - "
+					+ acc.getAmount()
+					+ " - "
+					+ acc.getDescription()
+					+ "\n";
+		}
+		
+		return format;
+	}
 }
